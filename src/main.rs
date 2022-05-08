ddf197d0b4e69521f4554ee9a20f36b8a0b1a06f
segfo <k.segfo@gmail.com>
2022-05-08 19:54:11 +0900

    FNV1を実装しました。

--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,10 @@
 use std::num::Wrapping;
-
-const OFFSET_BASIS32: usize = 2166136261;
-const OFFSET_BASIS64: usize = 14695981039346656037;
-const FNV_PRIME32: usize = 16777619;
-const FNV_PRIME_64: usize = 1099511628211;
-/**
- * 盤面をハッシュ化するための実装です。
- * FNV1アルゴリズムで実装されています。
- */
-struct FNV1;
-impl FNV1 {
-    fn hash64(cells: &Vec<Vec<Cell>>) -> usize {
-        let mut hash = OFFSET_BASIS64 as usize;
-        for y in 1..cells.len() - 1 {
-            for x in 1..cells[y].len() - 1 {
-                hash = (Wrapping(FNV_PRIME_64 as usize) * Wrapping(hash)).0
-                    ^ ((Wrapping(x + x) * Wrapping(y + y)).0
-                        * if cells[y][x].is_live() { 1 } else { 0 });
-            }
-        }
-        hash
-    }
-}
-
-/**
- * リングバッファの実装です。
- * このプログラムでは主に、盤面状況（ハッシュ値）の記録をします。
- */
-struct RingBuffer<T> {
-    buf: Vec<T>,
-    write: usize,    // 次の書き込み位置
-    read: usize,     // 次の読み取り位置
-    buf_size: usize, // 現在のリングバッファに書き込まれている有効なデータ数
-    // 上書きされたデータは無効なものとして扱う=buf_capacityを超えることはない
-    buf_capacity: usize, // 実際のリングバッファのキャパ
-}
-impl<T> RingBuffer<T>
-where
-    T: Clone + PartialEq + Copy,
-{
-    /**
-     * リングバッファを初期化します。
-     */
-    fn new(buffer_capacity: usize, init_data: T) -> Self {
-        RingBuffer {
-            buf: vec![init_data; buffer_capacity],
-            write: 0,
-            read: 0,
-            buf_size: 0,
-            buf_capacity: buffer_capacity,
-        }
-    }
-    /**
-     * リングバッファへデータを格納します。
-     * リングバッファの容量を超える場合は、古いデータから順に削除されます。
-     */
-    fn enqueue(&mut self, data: T) {
-        if self.buf_size < self.buf_capacity {
-            self.buf_size += 1;
-        } else {
-            self.read %= self.buf_capacity;
-            self.read += 1;
-        }
-        self.write %= self.buf_capacity;
-        self.buf[self.write] = data;
-        self.write += 1;
-    }
-    /**
-     * リングバッファからデータを取り出します。
-     * 取り出せない場合にはNoneが返ります。
-     */
-    fn dequeue(&mut self) -> Option<T> {
-        if self.buf_size > 0 {
-            self.buf_size -= 1;
-        } else {
-            return None;
-        }
-        self.read %= self.buf_capacity;
-        let data = self.buf[self.read];
-        self.read += 1;
-        Some(data)
-    }
-    /**
-     * リングバッファに `data` が存在するか確認します。
-     * 存在した場合には、trueが返却されます。
-     */
-    fn is_in_data(&self, data: T) -> bool {
-        self.buf.contains(&data)
-    }
-}
-
-/**
- * 盤面を表す実装です。
- * 盤面全体の状態を管理します。
- */
+use toolbox::{fnv1::FNV1, fnv1::FNV1_64, ring_buffer::RingBuffer};
+/// 盤面を表す実装です。
+/// 盤面全体の状態を管理します。
 struct Board {
     array: Vec<Vec<Cell>>,
-    old_hash: RingBuffer<usize>,
+    old_hash: RingBuffer<u64>,
     width: usize,
     height: usize,
 }
@@ -141,8 +49,18 @@ impl Board {
             .iter_mut()
             .for_each(|row| row.into_iter().for_each(|cell| cell.commit_state()));
     }
-    fn to_hash(&self) -> usize {
-        FNV1::hash64(&self.array)
+    fn to_hash(&self) -> u64 {
+        let mut fnv1 = FNV1_64::new();
+        for y in 1..self.array.len() - 1 {
+            for x in 1..self.array[y].len() - 1 {
+                let cell_data = ((Wrapping(x) * Wrapping(y)).0 as u64)
+                    * if self.array[y][x].is_live() { 1 } else { 0 };
+                for byte in cell_data.to_be_bytes() {
+                    fnv1.hash(byte);
+                }
+            }
+        }
+        fnv1.finalize()
     }
     fn show_board(&self) {
         for row in &self.array {
@@ -155,12 +73,11 @@ impl Board {
         println!("======================================");
     }
     fn is_done(&self) -> bool {
-        self.old_hash.is_in_data(self.to_hash())
+        self.old_hash.contains(self.to_hash())
     }
 }
-/**
- * Cellの状態を管理します。
- */
+
+/// Cellの状態を管理します。
 #[derive(Debug, Clone, PartialEq)]
 enum CellState {
     Dead,
